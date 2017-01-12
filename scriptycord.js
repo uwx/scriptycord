23001787b7260313f64012ada8ad5c4e6b78746e
rafa1231518 <chris331hansen@outlook.com>
2017-01-12 12:52:51 -0300

    direct extract + update

--- scriptycord.js
+++ scriptycord.js
@@ -3,14 +3,32 @@
 // TODO check for existance of app.asar... and if not then try applying to existing modified indexjs
 const path = require('path');
 const fs = require('fs');
+const asar = require('asar');
 
-const discordHome = String.raw`C:\Users\Rafael\AppData\Local\Discord\app-0.0.295\resources`;
+const home1 = process.env.LOCALAPPDATA + String.raw`\Discord\app-0.0.295\resources`;
+const home2 = process.env.LOCALAPPDATA + String.raw`\Discord\app-0.0.296\resources`;
+const home3 = process.env.LOCALAPPDATA + String.raw`\Discord\app-0.0.297\resources`;
+let discordHome;
 
-/*extract app.asar to app folder and rename it aside*/
+if (fs.existsSync(home3)) {
+  discordHome=home3;
+} else if (fs.existsSync(home2)) {
+  discordHome=home2;
+} else if (fs.existsSync(home1)) {
+  discordHome=home1;
+}
 
 process.chdir(discordHome);
 
-const cssPath = path.resolve('./css/css.css').replace(/\\/g, '\\\\');
+/*extract app.asar to app folder and rename it aside*/
+
+//console.log(proc.execSync('asar e app.asar app').toString());
+
+asar.extractAll('app.asar', './app');
+
+fs.renameSync('app.asar', 'bak_app.asar');
+
+const cssPath = path.resolve(process.env.LOCALAPPDATA + '/Discord/css/css.css').replace(/\\/g, '\\\\');
 
 const cssInjectionScript = `
 window._fs = require("fs");
@@ -44,9 +62,9 @@ window.applyAndWatchCSS = function(path) {
 };
 window.applyAndWatchCSS('${cssPath}');`;
 
-fs.writeFileSync('./app/app/cssInjection.js', cssInjectionScript);
+fs.writeFileSync('./app/cssInjection.js', cssInjectionScript);
 
-const cssInjectionScriptPath = path.resolve('./app/app/cssInjection.js').replace(/\\/g, '\\\\');
+const cssInjectionScriptPath = path.resolve('./app/cssInjection.js').replace(/\\/g, '\\\\');
 
 const cssReloadScript = `
 mainWindow.webContents.on('dom-ready', function () {
@@ -55,7 +73,7 @@ mainWindow.webContents.on('dom-ready', function () {
   );
 });`;
 
-const f = './app/app/index.js';
+const f = './app/index.js';
 const entireThing = fs.readFileSync(f, 'utf8');
 
 fs.writeFileSync(f, entireThing.replace("mainWindow.webContents.on('dom-ready', function () {});", cssReloadScript));
