b45dc4c5d37a6cc5f525d4e72e2698bf51b51587
cravxx <environskrakow@protonmail.com>
2018-05-16 15:24:45 -0500

    catch err when storage.json doesn't exist fix context menu for plugin settings

--- tree/injected/logic/storage.js
+++ tree/injected/logic/storage.js
@@ -5,7 +5,14 @@ const fs = Hansen.require('fsxt');
 const rootPath = window.hansenRoot;
 
 exports.load = async () => {
-  const storageData = JSON.parse(await fs.readFile(rootPath + '\\injectedUserData\\storage.json', 'utf8'));
+	const storageData = await fs.readFile(rootPath + '\\injectedUserData\\storage.json', 'utf8', function(err, data) {
+		if(err == null) {
+			console.log("[storage] loaded storage.json")
+			return data;
+		}
+		console.log("[storage] storage.json doesn't exist")
+		return {};
+	})
   window.addEventListener('unload', () => {
     fs.writeFileSync(rootPath + '\\injectedUserData\\storage.json', JSON.stringify(storageData));
   });
